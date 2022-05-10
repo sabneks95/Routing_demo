87ec489a850f767dfb346bf84cc6f3782042d8d4
sabneks95 <95006362+sabneks95@users.noreply.github.com>
2022-05-10 10:05:35 +0530

    Initial commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  faLock=faLock;
+
+  loginForm =new FormGroup({
+    
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+
+  }
+}
